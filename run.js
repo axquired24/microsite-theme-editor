03c456bbb313dc68b63adf82cf51f9e09438aa11
Albert S <albertseptiawan@ayoconnect.id>
2024-07-17 12:20:03 +0700

    fix: form (enable theme via form)

--- run.js
+++ run.js
@@ -6,6 +6,7 @@ const run = () => {
   createApp({
     // exposed to all expressions
     count: 0,
+    visibleContent: "site", // site/form
     detailTransaksi: [
       [
         "ID Transaksi",
@@ -40,9 +41,9 @@ const run = () => {
     ],
     theme: {
       color: {
-        primary: "red",
-        secondary: "green",
-        headerBg: "blue",
+        primary: "#FF3333",
+        secondary: "#018534",
+        headerBg: "#FF3333",
         headerTextColor: "light",
         buttonTextColor: "light"
       },
@@ -50,14 +51,75 @@ const run = () => {
         headerTitle: "Driver Special"
       }
     },
+    themeForm: {
+      headerTitle: "",
+      primaryColor: "",
+      secondaryColor: "",
+      headerBgColor: "",
+      headerTextColor: "",
+      buttonTextColor: ""
+    },
+    colorPickerModels: [
+      ["Header Background Color", "headerBgColor"],
+      ["Primary Color", "primaryColor"],
+      ["Secondary Color", "secondaryColor"]
+    ],
+    colorDropdownModels: [
+      ["Header Text Color", "headerTextColor"],
+      ["Button Text Color", "buttonTextColor"]
+    ],
+    currentThemeToForm() {
+      const currentTheme = JSON.parse(JSON.stringify(this.theme))
+      const newThemeForm = {
+        headerTitle: currentTheme.text.headerTitle,
+        primaryColor: currentTheme.color.primary,
+        secondaryColor: currentTheme.color.secondary,
+        headerBgColor: currentTheme.color.headerBg,
+        headerTextColor: currentTheme.color.headerTextColor,
+        buttonTextColor: currentTheme.color.buttonTextColor
+      }
+      this.themeForm = newThemeForm
+    },
+    applyThemeUsingForm() {
+      const currentForm = JSON.parse(JSON.stringify(this.themeForm))
+      const newTheme = {
+        color: {
+          primary: currentForm.primaryColor,
+          secondary: currentForm.secondaryColor,
+          headerBg: currentForm.headerBgColor,
+          headerTextColor: currentForm.headerTextColor,
+          buttonTextColor: currentForm.buttonTextColor
+        },
+        text: {
+          headerTitle: currentForm.headerTitle
+        }
+      }
+      this.theme = newTheme
+
+      return true
+    },
+    applyNewTheme() {
+      const isCorrect = this.applyThemeUsingForm()
+      if(isCorrect) {
+        this.visibleContent = "site"
+      } // endif
+    },
   
     // getters
     get plusOne() {
       return this.count + 1
     },
     // methods
-    increment() {
-      this.count++
+    toggleContent() {
+      if(this.visibleContent === "site") {
+        this.currentThemeToForm();
+      } // endif
+
+      const newContent = this.visibleContent === "site" ? "form" : "site"
+      this.visibleContent = newContent
+    },
+    darkLightToColor(param) {
+      return param == 'dark' ? '#000000' : '#FFFFFF';
     }
   }).mount()
 }
